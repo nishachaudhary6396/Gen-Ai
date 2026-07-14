999af89598ff767d894e01b2902da23b7acc6b1d
Nisha Chaudhary <nishachaudhary91101@gmail.com>
2026-07-14 16:54:46 +0530

    completed output parser

---
+++ langchain/output parser/JSONOutputParser.py
@@ -0,0 +1,22 @@
+from langchain_mistralai import ChatMistralAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+load_dotenv()
+
+model = ChatMistralAI()
+
+parser = JsonOutputParser()
+
+template = PromptTemplate(
+    template='Give me 5 facts about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic':'black hole'})
+
+print(result)
